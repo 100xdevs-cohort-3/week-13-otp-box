d42f3e2b368efd2139af921593dbad3ed3afe9eb
Adarsh <84severin@gmail.com>
2024-11-03 11:12:04 +0000

    Fixed backspace and some styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Otp } from './components/Otp'
 function App() {
 
   return (
-    <div className='h-screen bg-blue-700'>
+    <div className='h-screen w-screen bg-blue-700 flex justify-center item-center'>
       <br/><br/><br/><br/>
       <Otp number={20}/>
     </div>
